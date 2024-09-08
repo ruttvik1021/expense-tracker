01d48c3d62a835131d14abe1f62f3e5b9b221902
Ruttvik21 <rkhollam21@live.com>
2024-09-09 00:48:50 +0530

    token validity increased for register api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
   const token = jwt.sign(
     { userId: user._id },
     process.env.JWT_SECRET || "your-secret",
-    { expiresIn: "1h" }
+    { expiresIn: "24h" }
   );
 
   const headers = new Headers();
