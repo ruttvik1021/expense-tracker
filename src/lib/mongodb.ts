5d94dbed197279141d99c39c269639bca88daddf
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2024-09-11 15:25:59 +0530

    db connect

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export async function connectToDatabase = () => {
+export async function connectToDatabase() {
 
   if(mongoose.connections[0].readyState){
     return true
