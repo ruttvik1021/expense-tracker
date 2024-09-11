3581f09ae480ed016c03ac19eec220cb2417f9c7
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2024-09-11 13:39:49 +0530

    connection logic updated

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,7 +6,7 @@ type ConnectionObject = {
 
 const connection: ConnectionObject = {};
 
-async function dbConnect(): Promise<void> {
+async function connectToDatabase(): Promise<void> {
   // Check if we have a connection to the database or if it's currently connecting
   if (connection.isConnected) {
     console.log('Already connected to the database');
@@ -28,4 +28,4 @@ async function dbConnect(): Promise<void> {
   }
 }
 
-export default dbConnect;
+export default connectToDatabase;
