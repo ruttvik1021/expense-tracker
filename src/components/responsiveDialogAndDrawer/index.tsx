54ac68d8e426c87ca8e9ee4fe2273c2800989417
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2024-10-02 07:52:00 +0530

    Update index.tsx

--- src/components/responsiveDialogAndDrawer/index.tsx
+++ src/components/responsiveDialogAndDrawer/index.tsx
@@ -42,7 +42,7 @@ const ResponsiveDialogAndDrawer = ({
       </DialogContent>
     </Dialog>
   ) : (
-    <Drawer open={open} onDrag={handleClose} modal>
+    <Drawer open={open} onDrag={handleClose} modal={true}>
       {triggerButton && <DrawerTrigger asChild>{triggerButton}</DrawerTrigger>}
       <DrawerContent>
         <DrawerHeader className="text-left">
