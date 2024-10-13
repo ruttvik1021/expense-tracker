97654246450a4e9f36855a00be3a8cbe1a226a81
Ruttvik21 <rkhollam21@live.com>
2024-10-13 20:01:47 +0530

    drawer disabled

--- src/components/responsiveDialogAndDrawer/index.tsx
+++ src/components/responsiveDialogAndDrawer/index.tsx
@@ -6,14 +6,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+// import {
+//   Drawer,
+//   DrawerContent,
+//   DrawerDescription,
+//   DrawerHeader,
+//   DrawerTitle,
+//   DrawerTrigger,
+// } from "@/components/ui/drawer";
 import { useDeviceType } from "@/hooks/useMediaQuery";
 import React from "react";
 
@@ -42,26 +42,28 @@ const ResponsiveDialogAndDrawer = ({
       </DialogContent>
     </Dialog>
   ) : (
-    <Drawer open={open} onDrag={handleClose} disablePreventScroll modal>
-      {triggerButton && <DrawerTrigger asChild>{triggerButton}</DrawerTrigger>}
-      <DrawerContent className="rounded-3xl mx-4">
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{title}</DrawerTitle>
-          <DrawerDescription>{content}</DrawerDescription>
-        </DrawerHeader>
-      </DrawerContent>
-    </Drawer>
-    // {/* <div className="m-3">
-    //      <Dialog open={open}>
-    //       {triggerButton && <DialogTrigger asChild>{triggerButton}</DialogTrigger>}
-    //       <DialogContent className="rounded-3xl border" onClose={handleClose}>
-    //         <DialogHeader>
-    //           <DialogTitle>{title}</DialogTitle>
-    //           <DialogDescription>{content}</DialogDescription>
-    //         </DialogHeader>
-    //       </DialogContent>
-    //     </Dialog>
-    // </div> */}
+    // <Drawer open={open} onDrag={handleClose} disablePreventScroll modal>
+    //   {triggerButton && <DrawerTrigger asChild>{triggerButton}</DrawerTrigger>}
+    //   <DrawerContent className="rounded-3xl mx-4">
+    //     <DrawerHeader className="text-left">
+    //       <DrawerTitle>{title}</DrawerTitle>
+    //       <DrawerDescription>{content}</DrawerDescription>
+    //     </DrawerHeader>
+    //   </DrawerContent>
+    // </Drawer>
+    <div className="m-3">
+      <Dialog open={open}>
+        {triggerButton && (
+          <DialogTrigger asChild>{triggerButton}</DialogTrigger>
+        )}
+        <DialogContent className="rounded-3xl border" onClose={handleClose}>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{content}</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 
