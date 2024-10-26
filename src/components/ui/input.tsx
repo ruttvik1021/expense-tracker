f682164dc6376deedad4d968bfc0dc2c1c5b0900
Ruttvik21 <rkhollam21@live.com>
2024-10-26 21:22:46 +0530

    removed autofocus

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -22,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           )}
           ref={ref}
           {...props}
-          autoFocus={false}
+          autoFocus={undefined}
         />
         {type === "password" && (
           <button
