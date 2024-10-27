777767aacfd3f0da80862805d330c5d9e00b55b9
Ruttvik21 <rkhollam21@live.com>
2024-10-27 22:56:10 +0530

    ui updated

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -6,21 +6,27 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="container mx-auto p-4">
-      <div className="max-w-2xl mx-auto">
+    <div className="container flex flex-wrap mx-auto p-4 gap-3">
+      <div className="flex-1 max-w-2xl mx-auto">
         <Tabs defaultValue="profile" className="w-full">
           <TabsList className="grid w-full grid-cols-2 mb-4">
             <TabsTrigger value="profile">Profile</TabsTrigger>
             <TabsTrigger value="password">Password</TabsTrigger>
           </TabsList>
           <TabsContent value="profile">
-            <UpdateProfile />
-            <PaymentSources />
+            <div className="place-items-center w-full">
+              <UpdateProfile />
+            </div>
           </TabsContent>
           <TabsContent value="password">
-            <UpdatePassword />
+            <div className="place-items-center w-full">
+              <UpdatePassword />
+            </div>
           </TabsContent>
         </Tabs>
+      </div>
+      <div className="flex-1">
+        <PaymentSources />
       </div>
     </div>
   );
--- src/components/paymentSources/paymentSources.tsx
+++ src/components/paymentSources/paymentSources.tsx
@@ -7,13 +7,13 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { WalletMinimalIcon } from "lucide-react";
 import React from "react";
+import PageHeader from "../common/Pageheader";
 import CustomAddIcon from "../icons/customAddIcon";
 import CustomDeleteIcon from "../icons/customDeleteIcon";
 import CustomEditIcon from "../icons/customEditIcon";
 import { Input } from "../ui/input";
-import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
 import { useSourceMutation } from "./hooks/useSourcesMutation";
 import { useSourceById, useSources } from "./hooks/useSourcesQuery";
 
@@ -62,26 +62,22 @@ const PaymentSources = () => {
   };
 
   return (
-    <div className="container flex-col">
-      <div className="flex flex-wrap justify-around items-center gap-3 my-2">
-        <Label
-          htmlFor="payment source"
-          className="flex items-center space-x-2 text-gray-700"
-        >
-          <WalletMinimalIcon className="w-5 h-5" />
-          <span>Payment Source</span>
-        </Label>
-        <Input
-          type="text"
-          id="category"
-          placeholder="Source Name"
-          value={createSource}
-          onChange={(e) => setCreateSource(e.target.value)}
-          disabled={addSource.isPending}
-        />
+    <div className="container flex-col bg-drawer px-5 rounded-lg h-full">
+      <PageHeader title={"Payment Source"} />
+      <div className="flex flex-wrap justify-between items-center gap-3 my-3">
+        <div className="flex gap-3">
+          <Input
+            type="text"
+            id="category"
+            placeholder="Source Name"
+            value={createSource}
+            onChange={(e) => setCreateSource(e.target.value)}
+            disabled={addSource.isPending}
+          />
+        </div>
         <CustomAddIcon onClick={handleAddSource} type="TEXT" />
       </div>
-
+      <Separator />
       <div>
         <Table>
           <TableHeader>
@@ -92,75 +88,78 @@ const PaymentSources = () => {
           </TableHeader>
           <TableBody>
             {paymentSources?.map((source) => (
-              <TableRow key={source._id}>
-                <TableCell className="w-[80%]">
-                  {deleteSourceId === source._id ? (
-                    <span className="mr-2 text-red-600">
-                      Are you sure you want to delete {source.source}?
-                    </span>
-                  ) : editSourceId.id === source._id ? (
-                    <Input
-                      value={editSourceId.source}
-                      onChange={(e) =>
-                        setEditSourceId((prev) => ({
-                          ...prev,
-                          source: e.target.value,
-                        }))
-                      }
-                      className="max-w-sm"
-                    />
-                  ) : (
-                    source.source
-                  )}
-                </TableCell>
-                <TableCell className="flex gap-2">
-                  {deleteSourceId === source._id ? (
-                    <>
-                      <CustomAddIcon
-                        onClick={() => handleDeleteSource(source._id)}
-                        tooltip="Confirm"
-                        type="TEXT"
-                      />
-                      <CustomDeleteIcon
-                        onClick={() => setDeleteSourceId("")}
-                        tooltip="Cancel"
-                        type="TEXT"
-                      />
-                    </>
-                  ) : editSourceId.id === source._id ? (
-                    <>
-                      <CustomAddIcon
-                        onClick={handleEditSource}
-                        tooltip="Update"
-                        type="TEXT"
-                      />
-                      <CustomDeleteIcon
-                        onClick={() => setEditSourceId(initialEditSource)}
-                        tooltip="Cancel"
-                        type="TEXT"
-                      />
-                    </>
-                  ) : (
-                    <>
-                      <CustomEditIcon
-                        onClick={() =>
-                          setEditSourceId({
-                            id: source._id,
-                            source: source.source,
-                          })
+              <>
+                <TableRow key={source._id}>
+                  <TableCell className="w-[80%]">
+                    {deleteSourceId === source._id ? (
+                      <span className="mr-2 text-red-600">
+                        Are you sure you want to delete {source.source}?
+                      </span>
+                    ) : editSourceId.id === source._id ? (
+                      <Input
+                        value={editSourceId.source}
+                        onChange={(e) =>
+                          setEditSourceId((prev) => ({
+                            ...prev,
+                            source: e.target.value,
+                          }))
                         }
-                        tooltip="Edit"
-                        type="TEXT"
+                        className="max-w-sm"
                       />
-                      <CustomDeleteIcon
-                        onClick={() => setDeleteSourceId(source._id)}
-                        tooltip="Delete"
-                        type="TEXT"
-                      />
-                    </>
-                  )}
-                </TableCell>
-              </TableRow>
+                    ) : (
+                      source.source
+                    )}
+                  </TableCell>
+                  <TableCell className="flex gap-2">
+                    {deleteSourceId === source._id ? (
+                      <>
+                        <CustomAddIcon
+                          onClick={() => handleDeleteSource(source._id)}
+                          tooltip="Confirm"
+                          type="TEXT"
+                        />
+                        <CustomDeleteIcon
+                          onClick={() => setDeleteSourceId("")}
+                          tooltip="Cancel"
+                          type="TEXT"
+                        />
+                      </>
+                    ) : editSourceId.id === source._id ? (
+                      <>
+                        <CustomAddIcon
+                          onClick={handleEditSource}
+                          tooltip="Update"
+                          type="TEXT"
+                        />
+                        <CustomDeleteIcon
+                          onClick={() => setEditSourceId(initialEditSource)}
+                          tooltip="Cancel"
+                          type="TEXT"
+                        />
+                      </>
+                    ) : (
+                      <>
+                        <CustomEditIcon
+                          onClick={() =>
+                            setEditSourceId({
+                              id: source._id,
+                              source: source.source,
+                            })
+                          }
+                          tooltip="Edit"
+                          type="TEXT"
+                        />
+                        <CustomDeleteIcon
+                          onClick={() => setDeleteSourceId(source._id)}
+                          tooltip="Delete"
+                          type="TEXT"
+                        />
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+                <Separator />
+              </>
             ))}
           </TableBody>
         </Table>
