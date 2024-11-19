232d0d5cdee21ac916ce1c607ef099d18e24447d
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2024-11-19 10:13:34 +0530

    Update paymentSources.tsx

--- src/components/paymentSources/paymentSources.tsx
+++ src/components/paymentSources/paymentSources.tsx
@@ -66,7 +66,7 @@ const PaymentSources = () => {
             onChange={(e) => setCreateSource(e.target.value)}
             disabled={addSource.isPending}
           />
-          <CustomAddIcon onClick={handleAddSource} type="TEXT" />
+          <CustomAddIcon onClick={handleAddSource} type="TEXT" disabled={!createSource}/>
         </div>
       </div>
       <Separator />
