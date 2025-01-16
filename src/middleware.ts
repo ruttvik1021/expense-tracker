b18ad5604cb0b37e27f2df94c36ab34b795e7ebb
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2025-01-16 17:57:15 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,7 @@ import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("token"); // Use req.cookies.get to access cookies
-  const url = req.nextUrl.clone();
+  const token = req.cookies.get("token");
 
   const verifyEmailRoute = ["/verify-email"];
   const protectedRoutes = [
@@ -13,49 +12,21 @@ export async function middleware(req: NextRequest) {
     "/profile",
   ];
   const unprotectedRoutes = ["/", "/login", "/register"];
-
-  // Handle /verify-email route
+  
   if (verifyEmailRoute.includes(url.pathname)) {
-    url.pathname = token ? "/dashboard" : "/login";
-    return NextResponse.redirect(url);
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+  
+  if (protectedRoutes && token) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
-  if (protectedRoutes.includes(url.pathname)) {
-    if (token) {
-      try {
-        await jwtVerify(
-          token?.value,
-          new TextEncoder().encode(process.env.JWT_SECRET!)
-        );
-        return NextResponse.next(); // Token is valid, proceed as normal
-      } catch (err) {
-        url.pathname = "/login"; // If token verification fails, redirect to /login
-        return NextResponse.redirect(url);
-      }
-    }
-    url.pathname = "/login"; // If no token, redirect to /login
-    return NextResponse.redirect(url);
+  if (unprotectedRoutes && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
-  if (unprotectedRoutes.includes(url.pathname)) {
-    if (token) {
-      url.pathname = "/dashboard";
-      return NextResponse.redirect(url);
-    }
-    return NextResponse.next();
-  }
+  return NextResponse.next();
 }
-
-// Use explicit paths in the matcher
-export const config = {
-  matcher: [
-    "/verify-email",
-    "/category",
-    "/transaction",
-    "/dashboard",
-    "/profile",
-    "/",
-    "/login",
-    "/register",
-  ],
-};
