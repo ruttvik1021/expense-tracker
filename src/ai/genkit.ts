1c68e88e9d61931629a275817a37cd6b147eaec8
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2025-09-18 13:50:35 +0530

    changed model to googleai/gemini-2.5-flash-lite

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,6 @@ import { googleAI } from "@genkit-ai/googleai";
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: "googleai/gemini-2.5-flash",
+  // model: "googleai/gemini-2.5-flash",
+  model: "googleai/gemini-2.5-flash-lite",
 });
