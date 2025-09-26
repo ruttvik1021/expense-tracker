b55c8d20ebe5dbd90df5dc817089be41a3d7bacc
ruttvik1021 <93306636+ruttvik1021@users.noreply.github.com>
2025-09-26 18:56:20 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -59,6 +59,6 @@ export const config = {
     "/login",
     "/register",
     "/chat",
-    "/((?!_next/static|_next/image|favicon.ico|images|fonts|.*\\..*).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)",
   ],
 };
