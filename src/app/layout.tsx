616ca6e7d2513052ae5d054f002a78164fb7c596
ruttvik1021 <rkhollam21@live.com>
2025-11-15 14:54:05 +0530

    feat: update css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
